f8299e789fe97b3e0f0c280f25ff4ab0db182817
Ivan Sobolevskyi <ivan@osi.in.ua>
2023-04-26 13:01:58 +0300

    added go plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -5,7 +5,21 @@ return {
   {
     'nvim-lua/plenary.nvim'
   },
-  'ThePrimeagen/harpoon'
+  'ThePrimeagen/harpoon',
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  }
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
